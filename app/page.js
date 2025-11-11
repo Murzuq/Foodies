0e4b8115234300570ecdd8202f67c4c7d192d96c
Murzuq Alabi <murzuqalabi@gmail.com>
2025-11-11 08:46:50 +0100

    Added header to Landing page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,26 @@
+import Link from "next/link";
+
+import ImageSlideshow from "@/components/images/image-slideshow";
 import classes from "./page.module.css";
 
 export default function Home() {
   return (
     <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
+        <div>
+          <div className={classes.hero}>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Taste & share food from all over the world.</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
+      </header>
       <main>
         <section className={classes.section}>
           <h2>How it works</h2>
