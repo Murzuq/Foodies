3ff12b310924e32734f632d325341533a3f59b3b
Murzuq Alabi <murzuqalabi@gmail.com>
2025-11-12 10:08:40 +0100

    Updated Responsiveness

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,5 +1,8 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 import MainHeaderBackground from "./main-header-background";
 import logoImg from "@/assets/logo.png";
@@ -7,6 +10,12 @@ import classes from "./main-header.module.css";
 import NavLink from "./nav-link";
 
 export default function MainHeader() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
       <MainHeaderBackground />
@@ -15,6 +24,12 @@ export default function MainHeader() {
           <Image src={logoImg} alt="A plate with food on it" priority />
           Foodies
         </Link>
+
+        <button className={classes.hamburger} onClick={toggleMenu}>
+          <span />
+          <span />
+          <span />
+        </button>
 
         <nav className={classes.nav}>
           <ul>
@@ -26,6 +41,21 @@ export default function MainHeader() {
             </li>
           </ul>
         </nav>
+
+        {menuOpen && (
+          <div className={classes.mobileNavOverlay} onClick={toggleMenu}>
+            <nav className={classes.mobileNav}>
+              <ul>
+                <li>
+                  <NavLink href="/meals">Browse Meals</NavLink>
+                </li>
+                <li>
+                  <NavLink href="/community">Foodies Community</NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        )}
       </header>
     </>
   );
